52d1768ecce60f8e07771c928a852de1b24bd731
Anton Shchukin <a.a.shchukin@gmail.com>
2016-07-01 20:45:17 +0700

    update quotes style

--- gulpfile.js
+++ gulpfile.js
@@ -1,104 +1,104 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var precss = require("precss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync");
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var precss = require('precss');
+var autoprefixer = require('autoprefixer');
+var server = require('browser-sync');
 var mqpacker = require('css-mqpacker');
 var minify = require('gulp-csso');
-var imagemin = require("gulp-imagemin");
-var rename = require("gulp-rename");
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
 var svgmin = require('gulp-svgmin');
-var svgstore = require("gulp-svgstore");
-var del = require("del");
-var run = require("run-sequence");
+var svgstore = require('gulp-svgstore');
+var del = require('del');
+var run = require('run-sequence');
 
 
-gulp.task("clean", function() {
-  del("build");
+gulp.task('clean', function() {
+  del('build');
 });
 
-gulp.task("fonts", function() {
-  gulp.src("fonts/**/*")
+gulp.task('fonts', function() {
+  gulp.src('fonts/**/*')
     .pipe(plumber())
-    .pipe(gulp.dest("build/fonts"));
+    .pipe(gulp.dest('build/fonts'));
 });
 
 
-gulp.task("scripts", function() {
-  gulp.src("js/**/*")
+gulp.task('scripts', function() {
+  gulp.src('js/**/*')
     .pipe(plumber())
-    .pipe(gulp.dest("build/js"));
+    .pipe(gulp.dest('build/js'));
 });
 
-gulp.task("pages", function() {
-  gulp.src("*.html")
+gulp.task('pages', function() {
+  gulp.src('*.html')
     .pipe(plumber())
-    .pipe(gulp.dest("build"))
+    .pipe(gulp.dest('build'))
     .pipe(server.reload({stream: true}));
 });
 
-gulp.task("images", function() {
-  gulp.src("img/*.{png,jpg,gif}")
+gulp.task('images', function() {
+  gulp.src('img/*.{png,jpg,gif}')
     .pipe(plumber())
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true})
     ]))
-    .pipe(gulp.dest("build/img"));
+    .pipe(gulp.dest('build/img'));
 
-  gulp.src("img/*.svg") // not a sprite
+  gulp.src('img/*.svg') // not a sprite
     .pipe(plumber())
-    .pipe(gulp.dest("build/img"));
+    .pipe(gulp.dest('build/img'));
 });
 
-gulp.task("symbols", function() {
-  gulp.src("img/symbols/*.svg")
+gulp.task('symbols', function() {
+  gulp.src('img/symbols/*.svg')
     .pipe(plumber())
     .pipe(svgmin())
     .pipe(svgstore({
       inlineSvg: true
     }))
-    .pipe(rename("symbols.svg"))
-    .pipe(gulp.dest("build/img"));
+    .pipe(rename('symbols.svg'))
+    .pipe(gulp.dest('build/img'));
 });
 
-gulp.task("style", function() {
-  gulp.src("postcss/style.css")
+gulp.task('style', function() {
+  gulp.src('postcss/style.css')
     .pipe(plumber())
     .pipe(postcss([
       precss(),
       autoprefixer({browsers: [
-        "last 1 version",
-        "last 2 Chrome versions",
-        "last 2 Firefox versions",
-        "last 2 Opera versions",
-        "last 2 Edge versions"
+        'last 1 version',
+        'last 2 Chrome versions',
+        'last 2 Firefox versions',
+        'last 2 Opera versions',
+        'last 2 Edge versions'
       ]}),
       mqpacker({
         sort: true
       })
     ]))
     .pipe(minify())
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest('build/css'))
 
     .pipe(server.reload({stream: true}));
 });
 
-gulp.task("build", function (fn) {
-  run("clean", "style", "images", "symbols", "pages", "fonts", "scripts", fn);
+gulp.task('build', function (fn) {
+  run('clean', 'style', 'images', 'symbols', 'pages', 'fonts', 'scripts', fn);
 });
 
-gulp.task("serve", ["build"], function() {
+gulp.task('serve', ['build'], function() {
   server.init({
-    server: "build/",
+    server: 'build/',
     notify: false,
     open: true,
     ui: false
   });
 
-  gulp.watch("postcss/**/*.css", ["style"]);
-  gulp.watch("*.html", ["pages"]);
+  gulp.watch('postcss/**/*.css', ['style']);
+  gulp.watch('*.html', ['pages']);
 });
